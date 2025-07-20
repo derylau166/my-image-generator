b3da865f0765ffcb0423e87478cfdec09a0bc89f
derylau166 <derylau166@gmail.com>
2025-07-20 16:29:38 +0700

    Delete dictionary.js

--- dictionary.js
+++
@@ -1,9 +0,0 @@
-// lib/dictionary.js
-// import 'server-only' // Opsional: Pastikan ini hanya di server (untuk App Router)
-
-const dictionaries = {
-  en: () => import('../locales/en.json').then((module) => module.default),
-  id: () => import('../locales/id.json').then((module) => module.default),
-};
-
-export const getDictionary = async (locale) => dictionaries[locale]();
